8c0743c03bf7d3a15f7576e6cd6edca89d567d66
Kr-Anuj <155348877+Kr-Anuj@users.noreply.github.com>
2025-09-06 12:41:43 +0530

    Update user.routes.js
    Imported verfiCaptcha middle and added it to the register and login requests

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,23 +3,26 @@ import * as userController from '../controllers/user.controller.js';
 import * as projectController from '../controllers/project.controller.js';
 import { body } from "express-validator";
 import * as authMiddleware from '../middleware/auth.middleware.js';
+import { verifyCaptcha } from '../middleware/captcha.middleware.js';
 
 const router = Router();
 
 router.post('/register',
+    verifyCaptcha,
     body('email').isEmail().withMessage('Email must be a valid email address'),
-    body('password').isLength({ min:6 }).withMessage('Password must be at least 6 characters long'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
     userController.createUserController);
 
 router.post('/login',
+    verifyCaptcha,
     body('email').isEmail().withMessage('Email must be a valid email address'),
-    body('password').isLength({ min:6 }).withMessage('Password must be at least 6 characters long'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
     userController.loginController);
 
 router.get('/profile', authMiddleware.authUser, userController.profileController);
 
 router.get('/logout', authMiddleware.authUser, userController.logoutController);
-      
+
 router.get('/all', authMiddleware.authUser, userController.getAllUsersController);
 
 export default router;
